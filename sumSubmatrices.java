0c82b9cb330d8115e18021ddcedd2b7e5557ab2e
Shivam Singhal <swingle8@gmail.com>
2020-04-04 16:34:53 +0530

    Add files via upload

---
+++ sumSubmatrices.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int solve(int[][] A) {
+        int n = A.length;
+        int m = A[0].length;
+        int total = 0;
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < m ; j ++) {
+                int element = A[i][j];
+                int waysForL1 = i+1;
+                int whaysForL2 = n-i;
+                int waysForR1 = j+1;
+                int whaysForR2 = m-j;
+                
+                int totalWays = waysForL1*whaysForL2*waysForR1*whaysForR2;
+                int contri = totalWays * element;
+                total = total + contri;
+                
+            }
+        }
+        return total;
+    }
+    
+}
