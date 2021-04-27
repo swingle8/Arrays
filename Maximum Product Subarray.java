c5b30ef2d235156ca714ec1faa51e10f6611bd79
Shivam Singhal <swingle8@gmail.com>
2021-04-28 05:12:57 +0530

    Add files via upload

---
+++ Maximum Product Subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+       int ans = Integer.MIN_VALUE;
+       int mul = 1;
+            
+       for (int i = 0 ; i < nums.length ; i++) {
+            mul = mul * nums[i];
+            ans = Math.max(ans, mul);
+            if (mul == 0)
+                    mul = 1;
+       }
+        
+        mul = 1;
+        for (int i = nums.length-1 ; i >= 0 ; i--) {
+            mul = mul * nums[i];
+            
+            ans = Math.max(ans, mul);
+            if (mul == 0)
+                mul = 1;
+       }
+        
+        return ans;
+    }
+}
